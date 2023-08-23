023f2fd98b8aa1f20b690782fd65429f499a924e
Joshua Spitaleri <joshua.spitaleri@c02g695zml85.lan>
2023-08-23 11:05:04 -0400

    changed to windows

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ data "aws_ami" "windows_2022" {
   most_recent = true
 
   filter {
-    name   = "name"
+    name   = "ami"
     values = ["ami-07e70003c665fb5f3"]
   }
 
@@ -24,7 +24,6 @@ resource "aws_instance" "windows_2022" {
   instance_type               = var.instance_type
   subnet_id                   = var.existing_subnet_id
   associate_public_ip_address = true
-  key_name                    = pat-opa-aws-servers
 
   tags = {
     Name = var.instance_name
